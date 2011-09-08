7be6036cec337a26a8dbd753589d15d2422b847d
webshooter <webshooter@gmail.com>
2011-09-08 15:12:18 -0700

    +added log4j setup +added mbdb.xsd schema +added mbdb jaxb generater files +added default properties file

--- MediaBackup/src/com/rnickerson/code/mediabackup/core/MBApp.java
+++ MediaBackup/src/com/rnickerson/code/mediabackup/core/MBApp.java
@@ -1,13 +1,18 @@
 package com.rnickerson.code.mediabackup.core;
 
+import org.apache.log4j.Logger;
+
+
 public class MBApp {
+	
+	public static Logger logger = Logger.getLogger(MBApp.class.getName());
+	public static String FILESEP = System.getProperty("file.separator");
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
 	}
 
 }
