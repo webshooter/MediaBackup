80e6a2d8080f5785f16c533dfb0fa57026a90cca
webshooter <webshooter@gmail.com>
2011-09-08 12:42:20 -0700

    initial commit

---
+++ MediaBackup/src/com/rnickerson/code/mediabackup/core/MBApp.java
@@ -0,0 +1,13 @@
+package com.rnickerson.code.mediabackup.core;
+
+public class MBApp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
